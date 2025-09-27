d809036bd7faba8d18cdb39b4e9a4b0467a4a738
Parsa Ahmadi Nasab Emran <parsatempleuniversity@outlook.com>
2025-09-27 13:42:28 -0400

    Update authController to sanitize inputs using sanitize-html v2.17.0

---
+++ controllers/authController.js
@@ -0,0 +1,27 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
+const sanitizeHtml = require("sanitize-html");
+
+// @desc    Register user
+// @route   POST /api/auth/register
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const name = sanitizeHtml(req.body.name);
+    const email = req.body.email;
+    const password = req.body.password;
+
+    res.status(200).json({ message: `User input sanitized: ${name}, ${email}` });
+});
+
+// @desc    Login user
+// @route   POST /api/auth/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    res.status(200).json({ message: `Login input sanitized: ${email}` });
+});
+
+module.exports = { registerUser, authUser };
